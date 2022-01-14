9236eb45776437ea72a635080235385380dd95fd
Damian Porombka <dp@porombka-marketing.de>
2022-01-14 15:46:58 +0100

    Plugin Strikethrough wieder entfernt.

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -11,7 +11,6 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -41,7 +40,6 @@ ClassicEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
-	Strikethrough,
 	BlockQuote,
 	CKFinder,
 	CloudServices,
@@ -71,7 +69,6 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
-			'strikethrough',
 			'link',
 			'bulletedList',
 			'numberedList',
